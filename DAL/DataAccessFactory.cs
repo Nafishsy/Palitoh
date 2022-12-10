2530ffc7197898c19304026b74b0e86e0cfcaf99
Nafishsy <nafizahmed273273@gmail.com>
2022-12-11 01:42:40 +0600

    DataAccessFactory

--- DAL/DataAccessFactory.cs
+++ DAL/DataAccessFactory.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DAL.EF.Models;
+using DAL.Interfaces;
+using DAL.Repos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,27 @@ namespace DAL
 {
     internal class DataAccessFactory
     {
+        public static IRepo<MapCustomerVet, int, MapCustomerVet> MapCustomerVetDataAccess()
+        {
+            return new MapCustomerVetRepo();
+        }
+        public static IRepo<MapCustomerFood, int, MapCustomerFood> MapCustomerFoodDataAccess()
+        {
+            return new MapCustomerFoodRepo();
+        }
+
+        public static IRepo<MapCustomerPet, int, MapCustomerPet> MapCustomerPetDataAccess()
+        {
+            return new MapCustomerPetRepo();
+        }
+        public static IRepo<Vet, int, Vet> VetDataAccess()
+        {
+            return new VetRepo();
+        }
+
+        public static IRepo<Customer, int, Customer> CustomerDataAccess()
+        {
+            return new CustomerRepo();
+        }
     }
 }
