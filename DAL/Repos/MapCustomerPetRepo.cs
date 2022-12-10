5585267589f67154a21157091ef4599c3fc670f6
Nafishsy <nafizahmed273273@gmail.com>
2022-12-11 00:59:07 +0600

    CRUD done in DAL

--- DAL/Repos/MapCustomerPetRepo.cs
+++ DAL/Repos/MapCustomerPetRepo.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DAL.EF.Models;
+using DAL.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,38 @@ using System.Threading.Tasks;
 
 namespace DAL.Repos
 {
-    internal class MapCustomerPetRepo
+    internal class MapCustomerPetRepo : Repo, IRepo<MapCustomerPet, int, MapCustomerPet>
     {
+        public MapCustomerPet Add(MapCustomerPet obj)
+        {
+            db.MapCustomerPets.Add(obj);
+            if (db.SaveChanges() > 0) return obj;
+            return null;
+        }
+
+        public bool Delete(MapCustomerPet obj)
+        {
+            var dbobj = Get(obj.Id);
+            db.MapCustomerPets.Remove(dbobj);
+            return db.SaveChanges() > 0;
+        }
+
+        public List<MapCustomerPet> Get()
+        {
+            return db.MapCustomerPets.ToList();
+        }
+
+        public MapCustomerPet Get(int id)
+        {
+            return db.MapCustomerPets.Find(id);
+        }
+
+        public MapCustomerPet Update(MapCustomerPet obj)
+        {
+            var dbobj = Get(obj.Id);
+            db.Entry(dbobj).CurrentValues.SetValues(obj);
+            if (db.SaveChanges() > 0) return obj;
+            return null;
+        }
     }
 }
--- DAL/Repos/MapCustomerVetRepo.cs
+++ DAL/Repos/MapCustomerVetRepo.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DAL.EF.Models;
+using DAL.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,38 @@ using System.Threading.Tasks;
 
 namespace DAL.Repos
 {
-    internal class MapCustomerVetRepo
+    internal class MapCustomerVetRepo : Repo, IRepo<MapCustomerVet, int, MapCustomerVet>
     {
+        public MapCustomerVet Add(MapCustomerVet obj)
+        {
+            db.MapCustomerVets.Add(obj);
+            if (db.SaveChanges() > 0) return obj;
+            return null;
+        }
+
+        public bool Delete(MapCustomerVet obj)
+        {
+            var dbobj = Get(obj.Id);
+            db.MapCustomerVets.Remove(dbobj);
+            return db.SaveChanges() > 0;
+        }
+
+        public List<MapCustomerVet> Get()
+        {
+            return db.MapCustomerVets.ToList();
+        }
+
+        public MapCustomerVet Get(int id)
+        {
+            return db.MapCustomerVets.Find(id);
+        }
+
+        public MapCustomerVet Update(MapCustomerVet obj)
+        {
+            var dbobj = Get(obj.Id);
+            db.Entry(dbobj).CurrentValues.SetValues(obj);
+            if (db.SaveChanges() > 0) return obj;
+            return null;
+        }
     }
 }
