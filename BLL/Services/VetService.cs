3cad0f6c4a06ef1d4b8cd9f651989d34250d0d3b
Nafishsy <nafizahmed273273@gmail.com>
2022-12-11 17:22:57 +0600

    Made Interface IRepo public, Created an ADD service in VetService

--- BLL/Services/VetService.cs
+++ BLL/Services/VetService.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using AutoMapper;
+using BLL.DTOs;
+using DAL;
+using DAL.EF.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +12,20 @@ namespace BLL.Services
 {
     internal class VetService
     {
+        public static VetDTO Add(VetDTO veternarian)
+        {
+            var cfg = new MapperConfiguration(c => {
+                c.CreateMap<VetDTO, Vet>();
+                c.CreateMap<Vet, VetDTO>();
+            });
+            var mapper = new Mapper(cfg);
+            var vet = mapper.Map<Vet>(veternarian);
+            var data = DataAccessFactory.VetDataAccess().Add(vet);
+
+            if (data != null) return mapper.Map<VetDTO>(data);
+            return null;
+        }
+
+
     }
 }
