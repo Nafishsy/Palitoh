88ad5a7c509eaa0ff91119f7638dba2af6c8fbb2
Nafishsy <nafizahmed273273@gmail.com>
2022-12-11 17:52:37 +0600

    Vet, Customers Service done

--- BLL/Services/VetService.cs
+++ BLL/Services/VetService.cs
@@ -26,6 +26,41 @@ namespace BLL.Services
             return null;
         }
 
+        public static List<VetDTO> Get()
+        {
+            var data = DataAccessFactory.VetDataAccess().Get();
+            var cfg = new MapperConfiguration(c => {
+                c.CreateMap<Vet, VetDTO>();
+
+            });
+            var mapper = new Mapper(cfg);
+            return mapper.Map<List<VetDTO>>(data);
+        }
+
+        public static VetDTO Get(int id)
+        {
+            var data = DataAccessFactory.VetDataAccess().Get(id);
+            var cfg = new MapperConfiguration(c => {
+                c.CreateMap<Vet, VetDTO>();
+
+            });
+            var mapper = new Mapper(cfg);
+            return mapper.Map<VetDTO>(data);
+        }
+        public static VetsCustomerDTO GetAllAppoinments(int id)
+        {
+            //Aikhane Akta Vet er jonno or shob Customer/Paitent Der info Ashbe
+            var data = DataAccessFactory.VetDataAccess().Get(id);
+
+            var cfg = new MapperConfiguration(c => {
+                c.CreateMap<Vet, VetsCustomerDTO>();
+                c.CreateMap<Customer, CustomerDTO>();
+
+            });
+            var mapper = new Mapper(cfg);
+            return mapper.Map<VetsCustomerDTO>(data);
+        }
+
 
     }
 }
