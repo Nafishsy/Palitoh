ad6ee14b113e5848d5c139e027e2f0a6d984e2c6
anikkumarsaha <anikkumarofficial08@gmail.com>
2022-12-21 19:36:33 +0600

    kochir part api crud

--- Palitoh/Controllers/EmployeeController.cs
+++ Palitoh/Controllers/EmployeeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BLL.DTOs;
+using BLL.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +11,60 @@ namespace Palitoh.Controllers
 {
     public class EmployeeController : ApiController
     {
+        [Route("api/Employees")]
+        [HttpGet]
+        public HttpResponseMessage GetAllEmployees()
+        {
+            var data = EmployeeService.GetAllEmployees();
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+        }
+
+        [Route("api/Employee/{id}")]
+        [HttpGet]
+        public HttpResponseMessage GetEmployee(int id)
+        {
+            var data = EmployeeService.GetEmployee(id);
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+        }
+
+        [Route("api/Employee/delete")]
+        [HttpPost]
+        public HttpResponseMessage DeleteEmployee(EmployeeDTO obj)
+        {
+            var data = EmployeeService.DeleteEmployee(obj);
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+        }
+
+        [Route("api/Employee/add")]
+        [HttpPost]
+        public HttpResponseMessage AddEmployee(EmployeeDTO obj)
+        {
+            if (ModelState.IsValid)
+            {
+                var res = EmployeeService.AddEmployee(obj);
+                if (res != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, new { Msg = "Added", data = res });
+                }
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+            return Request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+        }
+
+        [Route("api/Employee/edit")]
+        [HttpPost]
+        public HttpResponseMessage EditEmployee(EmployeeDTO obj)
+        {
+            if (ModelState.IsValid)
+            {
+                var res = EmployeeService.EditEmployee(obj);
+                if (res != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, new { Msg = "Edited", data = res });
+                }
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+            return Request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+        }
     }
 }
--- Palitoh/Controllers/ShopController.cs
+++ Palitoh/Controllers/ShopController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BLL.DTOs;
+using BLL.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +11,179 @@ namespace Palitoh.Controllers
 {
     public class ShopController : ApiController
     {
+        //Shop Table
+        [Route("api/Shops")]
+        [HttpGet]
+        public HttpResponseMessage GetAllShops()
+        {
+            var data = ShopService.GetAllShops();
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+        }
+
+        [Route("api/Shop/{id}")]
+        [HttpGet]
+        public HttpResponseMessage GetShop(int id)
+        {
+            var data = ShopService.GetShop(id);
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+        }
+
+        [Route("api/Shop/delete")]
+        [HttpPost]
+        public HttpResponseMessage DeleteShop(ShopDTO obj)
+        {
+            var data = ShopService.DeleteShop(obj);
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+        }
+
+        [Route("api/Shop/add")]
+        [HttpPost]
+        public HttpResponseMessage AddShop(ShopDTO obj)
+        {
+            if (ModelState.IsValid)
+            {
+                var res = ShopService.AddShop(obj);
+                if (res != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, new { Msg = "Added", data = res });
+                }
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+            return Request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+        }
+
+        [Route("api/Shop/edit")]
+        [HttpPost]
+        public HttpResponseMessage EditShop(ShopDTO obj)
+        {
+            if (ModelState.IsValid)
+            {
+                var res = ShopService.EditShop(obj);
+                if (res != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, new { Msg = "Edited", data = res });
+                }
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+            return Request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+        }
+
+
+
+        //Pet Table
+        [Route("api/Pets")]
+        [HttpGet]
+        public HttpResponseMessage GetAllPets()
+        {
+            var data = PetService.GetAllPets();
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+        }
+
+        [Route("api/Pet/{id}")]
+        [HttpGet]
+        public HttpResponseMessage GetPet(int id)
+        {
+            var data = PetService.GetPet(id);
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+        }
+
+        [Route("api/Pet/delete")]
+        [HttpPost]
+        public HttpResponseMessage DeletePet(PetDTO obj)
+        {
+            var data = PetService.DeletePet(obj);
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+        }
+
+        [Route("api/Pet/add")]
+        [HttpPost]
+        public HttpResponseMessage AddPet(PetDTO obj)
+        {
+            if (ModelState.IsValid)
+            {
+                var res = PetService.AddPet(obj);
+                if (res != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, new { Msg = "Added", data = res });
+                }
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+            return Request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+        }
+
+        [Route("api/Pet/edit")]
+        [HttpPost]
+        public HttpResponseMessage EditPet(PetDTO obj)
+        {
+            if (ModelState.IsValid)
+            {
+                var res = PetService.EditPet(obj);
+                if (res != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, new { Msg = "Edited", data = res });
+                }
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+            return Request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+        }
+
+
+
+        //Food Table
+        [Route("api/Foods")]
+        [HttpGet]
+        public HttpResponseMessage GetAllFoods()
+        {
+            var data = FoodService.GetAllFoods();
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+        }
+
+        [Route("api/Food/{id}")]
+        [HttpGet]
+        public HttpResponseMessage GetFood(int id)
+        {
+            var data = FoodService.GetFood(id);
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+        }
+
+        [Route("api/Food/delete")]
+        [HttpPost]
+        public HttpResponseMessage DeleteFood(FoodDTO obj)
+        {
+            var data = FoodService.DeleteFood(obj);
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+        }
+
+        [Route("api/Food/add")]
+        [HttpPost]
+        public HttpResponseMessage AddFood(FoodDTO obj)
+        {
+            if (ModelState.IsValid)
+            {
+                var res = FoodService.AddFood(obj);
+                if (res != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, new { Msg = "Added", data = res });
+                }
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+            return Request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+        }
+
+        [Route("api/Food/edit")]
+        [HttpPost]
+        public HttpResponseMessage EditFood(FoodDTO obj)
+        {
+            if (ModelState.IsValid)
+            {
+                var res = FoodService.EditFood(obj);
+                if (res != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, new { Msg = "Edited", data = res });
+                }
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+            return Request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+        }
     }
 }
